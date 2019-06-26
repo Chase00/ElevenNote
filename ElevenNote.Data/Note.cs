05ee599a33b7795ff842e4784b4545819ede8f57
Chase Kuhn <chasekuhnmc@gmail.com>
2019-06-26 11:21:08 -0400

    Added the Note DB class

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    class Note
+    {
+        [Key]
+        [Display(Name = "NOTE4")]
+        public int NoteId { get; set; }
+
+        [Required]
+        [Display(Name = "OWNER4")]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        [Display(Name = "TITLE4")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "CONTENT4")]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
