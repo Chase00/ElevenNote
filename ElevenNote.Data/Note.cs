5741b97c6ba7fa2df38b671ab6a96f39a2adea26
Chase Kuhn <chasekuhnmc@gmail.com>
2019-06-27 11:11:35 -0400

    Added a staring notes feature

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -10,20 +11,23 @@ namespace ElevenNote.Data
     public class Note
     {
         [Key]
-        [Display(Name = "NOTE4")]
+        [Display(Name = "Note")]
         public int NoteId { get; set; }
 
         [Required]
-        [Display(Name = "OWNER4")]
+        [Display(Name = "Owner")]
         public Guid OwnerId { get; set; }
 
         [Required]
-        [Display(Name = "TITLE4")]
+        [Display(Name = "Title")]
         public string Title { get; set; }
 
         [Required]
-        [Display(Name = "CONTENT4")]
+        [Display(Name = "Content")]
         public string Content { get; set; }
+
+        [DefaultValue(false)]
+        public bool IsStarred { get; set; }
 
         [Required]
         public DateTimeOffset CreatedUtc { get; set; }
